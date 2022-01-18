ce43640cbe0f6d3f91263e2074d2a29e06176c86
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-01-18 16:28:54 +0100

    add simple sidebar

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/app.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>homepage</title>
+</head>
+<body>
+    <main class="container">
+        <aside class="sidebar">
+            <div>
+                <a href="/">Logo</a>
+                <nav class="sidebar__navigation">
+                    <div class="sidebar__navigation-docs">
+                        <ul>
+                            <li>
+                                <h2>Home</h2>
+                                <ul>
+                                    <li><a href="/">link 1</a></li>
+                                    <li><a href="/">link 2</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <h2>Feedback</h2>
+                                <ul>
+                                    <li><a href="/">link 1</a></li>
+                                    <li><a href="/">link 2</a></li>
+                                    <li><a href="/">link 3</a></li>
+                                    <li><a href="/">link 4</a></li>
+                                    <li><a href="/">link 5</a></li>
+                                    <li><a href="/">link 6</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <h2>Payments</h2>
+                                <ul>
+                                    <li><a href="/">link 1</a></li>
+                                    <li><a href="/">link 2</a></li>
+                                    <li><a href="/">link 3</a></li>
+                                    <li><a href="/">link 4</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </aside>
+        <section class="content">content</section>
+    </main>
+</body>
+</html>
